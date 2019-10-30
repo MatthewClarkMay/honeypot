48ee3c3ca2f6f91971e822318bb0e500b4a3a225
Matthew May <mcmay.web@gmail.com>
2019-10-29 19:27:34 -0700

    policy.bat
    sdf

---
+++ policy.bat
@@ -0,0 +1,9 @@
+AuditPol /set /category:"System" /success:disable /failure:disable
+AuditPol /set /category:"Logon/Logoff" /success:disable /failure:disable
+AuditPol /set /category:"Object Access" /success:disable /failure:disable
+AuditPol /set /category:"Privilege Use" /success:disable /failure:disable
+AuditPol /set /category:"Detailed Tracking" /success:disable /failure:disable
+AuditPol /set /category:"Policy Change" /success:disable /failure:disable
+AuditPol /set /category:"Account Management" /success:disable /failure:disable
+AuditPol /set /category:"DS Access" /success:disable /failure:disable
+AuditPol /set /category:"Account Logon" /success:disable /failure:disable
