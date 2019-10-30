98c2e35f79b8dc5bdf6c2d571db16728fece51be
Matthew May <mcmay.web@gmail.com>
2019-10-29 19:39:48 -0700

    Update policy.bat

--- policy.bat
+++ policy.bat
@@ -7,3 +7,27 @@ AuditPol /set /category:"Policy Change" /success:disable /failure:disable
 AuditPol /set /category:"Account Management" /success:disable /failure:disable
 AuditPol /set /category:"DS Access" /success:disable /failure:disable
 AuditPol /set /category:"Account Logon" /success:disable /failure:disable
+
+AuditPol /set /category:"System" /success:enable /failure:enable
+
+AuditPol /set /category:"Logon/Logoff" /success:enable /failure:enable
+AuditPol /set /subcategory:"IPsec Extended Mode" /success:disable /failure:disable
+AuditPol /set /subcategory:"IPsec Main Mode" /success:disable /failure:disable
+AuditPol /set /subcategory:"IPsec Quick Mode" /success:disable /failure:disable
+AuditPol /set /subcategory:"User / Device Claims" /success:disable /failure:disable
+
+AuditPol /set /subcategory:"File Share" /success:enable /failure:enable
+AuditPol /set /subcategory:"Sensitive Privilege Use" /success:enable /failure:enable
+
+AuditPol /set /subcategory:"RPC Events" /success:enable /failure:enable
+AuditPol /set /subcategory:"Token Right adjusted Events" /success:enable /failure:enable
+
+AuditPol /set /category:"Policy Change" /success:enable /failure:enable
+AuditPol /set /subcategory:"MPSSVC Rule-Level Policy Change" /success:disable /failure:disable
+AuditPol /set /subcategory:"Other Policy Change Events" /success:disable /failure:disable
+
+AuditPol /set /category:"Account Management" /success:enable /failure:enable
+
+AuditPol /set /subcategory:"Directory Service Changes" /success:enable /failure:enable
+
+AuditPol /set /category:"Account Logon" /success:enable /failure:enable
